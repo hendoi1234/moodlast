63d58e730f8cd4cd0f50619ef69bed498d2ecdc4
hendoi1234 <69905850+hendoi1234@users.noreply.github.com>
2020-11-01 10:03:18 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".circle").on("click", function () {
+        $(".flower1").addClass("disap")
+        $(".flower2").addClass("disap2")
+        $(".flower3").addClass("disap3")
+        $(".flower4").addClass("disap4")
+        $(".flower5").addClass("disap5")
+        $(".flower6").addClass("disap6")
+        $(".flower7").addClass("disap7")
+        $(".flower8").addClass("disap8")
+        $(".flower9").addClass("disap9")
+        $(".flower10").addClass("disap10")
+        $(".img").addClass("imgcome")
+        $(".color").addClass("disap11")
+        $(".white_title").addClass("white_come")
+        $(".black_title").addClass("black_come")
+
+    })
+
+
+})
+
